28e5c40729fe3ac486e47e3d1ea89d4038554f25
Oliver <63046532+Olive-Roe@users.noreply.github.com>
2023-10-12 12:36:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const form = document.querySelector("form")
 
+form.addEventListener("submit", (e)=>{
+  e.preventDefault()
+  const username = form.username.value
+  const password = form.password.value
+
+  const auth = authentication(username, password)
+
+  if (auth) {
+    alert("correct")
+  }
+  else {
+    alert("wrong")
+  }
+}
+
+function authentication(username, password) {
+  if (username === "admin" && password == "password") {
+    return true
+  }
+  else {
+    return false
+  }
+}
